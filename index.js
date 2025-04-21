848f99a5e2c43528d18ad25eeebcd37a06114aec
Daniela Pech <randakova.daniela@gmail.com>
2025-04-21 17:50:28 +0200

    casovac, posluchac udalosti, fce

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log('funguju!');
+
+const budik = document.querySelector('.alarm');
+
+const cas = prompt('Kolik vteřin má uplynout, než začne zvonit?');
+const sekundy = cas * 1000;
+
+const zvuk = document.querySelector('audio');
+
+const zvoneni = () => {
+  budik.classList.add('alarm--ring');
+  zvuk.play();
+};
+
+const spustitZvoneni = setTimeout(zvoneni, sekundy);
+
+const tlacitko = document.querySelector('button');
+const vypnoutBudik = () => {
+  clearTimeout(spustitZvoneni);
+};
+tlacitko.addEventListener('click', vypnoutBudik);
